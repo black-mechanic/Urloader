17137a8f355f5b660798573cd0f584f042d8e94a
black-mechanic <andreynuh@gmail.com>
2018-11-29 14:42:10 +0800

    Convert class to using pImpl template

--- src/urloader.cpp
+++ src/urloader.cpp
@@ -1,70 +1,74 @@
 #include "urloader.hpp"
-//#include <fstream>
+#include <fstream>
+#include <curl/curl.h>
 
 namespace urloader
 {
     struct Urloader::Impl{
+  /* Convert std::string to char*. Create vector<char> c_url. And will use it as &c_url[0]*/
+        std::vector<char> FillUrlfromString(const std::string& url_of_file){
+            std::vector<char> c_url(url_of_file.begin(), url_of_file.end());
+            c_url.push_back('\0');
+            return c_url;
+        }
+    // -----------* Callbacks *----------------------------------------
+        static size_t WriteToRAMCallback(char* current_chunk_of_data, size_t multiplier_size,
+                                            size_t chunk_size, std::vector<uint8_t>* big_storage) {
+            int bytes_in_chunk = multiplier_size * chunk_size;
+            if(big_storage == NULL){
+                return 0;
+            }
+            for(int i = 0; i< bytes_in_chunk; i++){
+                big_storage->push_back(current_chunk_of_data[i]);
+            }
+            return bytes_in_chunk;
+        }
+
+        static size_t WriteToFileCallback(char* current_chunk_of_data, size_t multiplier_size,
+                                            size_t chunk_size, std::fstream* big_storage) {
+            if(big_storage == NULL){
+                return 0;
+            }
+            big_storage->write(current_chunk_of_data, multiplier_size * chunk_size);
+            //big_storage->append(current_chunk_of_data, (multiplier_size * chunk_size));
+            return multiplier_size*chunk_size;
+        }
+
         CURL *curl_handler;
-    }
+        CURLcode curl_res;
+
+    };
 
 
     Urloader::Urloader() : mImpl(new Impl){
         /* init libcurl */
         curl_global_init(CURL_GLOBAL_ALL);
         /* init the curl session */
-        curl_handler = curl_easy_init();
+        mImpl->curl_handler = curl_easy_init();
     }
 
     Urloader::~Urloader(){
     /* cleanup curl stuff */
-        curl_easy_cleanup(curl_handler);
+        curl_easy_cleanup(mImpl->curl_handler);
     /* we're done with libcurl, so clean it up */
         curl_global_cleanup();
     }
-// -----------* Callbacks *----------------------------------------
-    size_t Urloader::WriteToRAMCallback(char* current_chunk_of_data, size_t multiplier_size,
-                                        size_t chunk_size, std::vector<uint8_t>* big_storage) {
-        int bytes_in_chunk = multiplier_size * chunk_size;
-        if(big_storage == NULL){
-            return 0;
-        }
-        for(int i = 0; i< bytes_in_chunk; i++){
-            big_storage->push_back(current_chunk_of_data[i]);
-        }
-        //append(current_chunk_of_data, (bytes_in_chunk));
-        return bytes_in_chunk;
-    }
 
-    size_t Urloader::WriteToFileCallback(char* current_chunk_of_data, size_t multiplier_size,
-                                        size_t chunk_size, std::fstream* big_storage) {
-        if(big_storage == NULL){
-            return 0;
-        }
-        big_storage->write(current_chunk_of_data, multiplier_size * chunk_size);
-        //big_storage->append(current_chunk_of_data, (multiplier_size * chunk_size));
-        return multiplier_size*chunk_size;
-    }
-// -----------* Helpers *------------------------------------------
-    /* Convert std::string to char*. Create vector<char> c_url. And will use it as &c_url[0]*/
-    std::vector<char> Urloader::FillUrlfromString(const std::string& url_of_file){
-        //std::vector<char> c_url;
-        std::vector<char> c_url(url_of_file.begin(), url_of_file.end());
-        //c_url = (url_of_file.begin(), url_of_file.end());
-        c_url.push_back('\0');
-        return c_url;
-    }
+
+
+
 // -----------* Main functions *------------------------------------
     /* Return True if remote resource exist and accessible */
     bool Urloader::IsUrlAccessible(const std::string& url_of_file){
-        std::vector<char> c_url = FillUrlfromString(url_of_file);
-        curl_easy_setopt(curl_handler, CURLOPT_URL, &c_url[0]);
+        std::vector<char> c_url = mImpl->FillUrlfromString(url_of_file);
+        curl_easy_setopt(mImpl->curl_handler, CURLOPT_URL, &c_url[0]);
         /* Now specify the HEAD method */
-        curl_easy_setopt(curl_handler, CURLOPT_NOBODY, 1L);
+        curl_easy_setopt(mImpl->curl_handler, CURLOPT_NOBODY, 1L);
         /* Perform the request, res will get the return code */
-        curl_res = curl_easy_perform(curl_handler);
-        curl_easy_reset(curl_handler);
+        mImpl->curl_res = curl_easy_perform(mImpl->curl_handler);
+        curl_easy_reset(mImpl->curl_handler);
         /* Check for errors */
-        if(curl_res != CURLE_OK){
+        if(mImpl->curl_res != CURLE_OK){
             return false;
         }else{
             return true;
@@ -74,26 +78,27 @@ namespace urloader
     /* Get string in RAM from URL; Input: HTTP URL of file; Output: file in buffer as a string */
     bool Urloader::GetManifestByUrl(const std::string& url_of_file, std::vector<uint8_t>& buffer){
 
-        std::vector<char> c_url = FillUrlfromString(url_of_file);
+        std::vector<char> c_url = mImpl->FillUrlfromString(url_of_file);
 
         /* specify URL to get */
-        curl_easy_setopt(curl_handler, CURLOPT_URL, &c_url[0]);
+        curl_easy_setopt(mImpl->curl_handler, CURLOPT_URL, &c_url[0]);
 
         /* save data to this buffer */
-        curl_easy_setopt(curl_handler, CURLOPT_WRITEDATA, &buffer);
+        curl_easy_setopt(mImpl->curl_handler, CURLOPT_WRITEDATA, &buffer);
 
         /* send all data to this function  */
-        curl_easy_setopt(curl_handler, CURLOPT_WRITEFUNCTION, Urloader::WriteToRAMCallback);
+        //curl_easy_setopt(curl_handler, CURLOPT_WRITEFUNCTION, Urloader::WriteToRAMCallback);
+        curl_easy_setopt(mImpl->curl_handler, CURLOPT_WRITEFUNCTION, mImpl->WriteToRAMCallback);
 
         /* some servers don't like requests that are made without a user-agent
              field, so we provide one */
-        curl_easy_setopt(curl_handler, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+        curl_easy_setopt(mImpl->curl_handler, CURLOPT_USERAGENT, "libcurl-agent/1.0");
 
         /* get it! */
-        curl_res = curl_easy_perform(curl_handler);
+        mImpl->curl_res = curl_easy_perform(mImpl->curl_handler);
         /* transmission done */
-        curl_easy_reset(curl_handler);
-        if(curl_res != CURLE_OK){
+        curl_easy_reset(mImpl->curl_handler);
+        if(mImpl->curl_res != CURLE_OK){
             return false;
         }else{
             return true;
@@ -102,33 +107,37 @@ namespace urloader
 
     bool Urloader::GetFileByUrl(const std::string& url_of_file, const std::string& path_to_file){
 
-        std::vector<char> c_url = FillUrlfromString(url_of_file);
+        std::vector<char> c_url = mImpl->FillUrlfromString(url_of_file);
 
         /* specify URL to get */
-        curl_easy_setopt(curl_handler, CURLOPT_URL, &c_url[0]);
+        curl_easy_setopt(mImpl->curl_handler, CURLOPT_URL, &c_url[0]);
 
         std::fstream file_handler;
         file_handler.open(path_to_file, std::fstream::out       |
                                         std::fstream::binary    |
                                         std::fstream::trunc);
 
-        curl_easy_setopt(curl_handler, CURLOPT_WRITEDATA, &file_handler);
+        curl_easy_setopt(mImpl->curl_handler, CURLOPT_WRITEDATA, &file_handler);
 
         /* send all data to this function  */
-        curl_easy_setopt(curl_handler, CURLOPT_WRITEFUNCTION, Urloader::WriteToFileCallback);
+        curl_easy_setopt(mImpl->curl_handler, CURLOPT_WRITEFUNCTION, mImpl->WriteToFileCallback);
 
         /* some servers don't like requests that are made without a user-agent
              field, so we provide one */
-        curl_easy_setopt(curl_handler, CURLOPT_USERAGENT, "libcurl-agent/1.0");
-        curl_res = curl_easy_perform(curl_handler);
+        curl_easy_setopt(mImpl->curl_handler, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+        mImpl->curl_res = curl_easy_perform(mImpl->curl_handler);
         file_handler.close();
         /* transmission done */
-        curl_easy_reset(curl_handler);
-        if(curl_res != CURLE_OK){
+        curl_easy_reset(mImpl->curl_handler);
+        if(mImpl->curl_res != CURLE_OK){
             return false;
         }else{
             return true;
         }
+    }
+
+    uint32_t Urloader::GetLastResultCode(){
+        return static_cast<uint32_t>(mImpl->curl_res);
     }
 
 
--- src/urloader.hpp
+++ src/urloader.hpp
@@ -1,10 +1,8 @@
 #ifndef URLOADER_HPP
 #define URLOADER_HPP
 
-#include <curl/curl.h>
 #include <string>
 #include <vector>
-#include <fstream>
 #include <memory>
 
 namespace urloader
@@ -18,21 +16,11 @@ namespace urloader
         bool IsUrlAccessible(const std::string& url_of_file);
         bool GetManifestByUrl(const std::string& url_of_file, std::vector<uint8_t>& buffer  );
         bool GetFileByUrl(const std::string& url_of_file, const std::string& path_to_file);
-
-        CURLcode curl_res;
-
+        uint32_t GetLastResultCode(void);
 
     private:
         struct Impl;
         std::unique_ptr<Impl> mImpl;
-        std::vector<char> FillUrlfromString(const std::string& url_of_file);
-        static size_t WriteToRAMCallback(char *current_chunk_of_data, size_t multiplier_size,
-                                         size_t chunk_size, std::vector<uint8_t>* big_storage);
-        static size_t WriteToFileCallback(char* current_chunk_of_data, size_t multiplier_size,
-                                                    size_t chunk_size, std::fstream* big_file);
-
-
-
     };
 }
 #endif // URLOADER_HPP
